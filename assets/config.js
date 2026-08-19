d491e3cc7342c9fbdce4dac6799d163e0a0f7384
Janine Glathar <jlg046@bucknell.edu>
2026-08-19 12:41:41 -0400

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 // Replace these two values with the values from your Supabase project.
 // Supabase Dashboard -> Project Settings -> API
 window.GISPE_CONFIG = {
-  supabaseUrl: "https://nxgaznpdocddjzqmjgtd.supabase.co/rest/v1/",
+  supabaseUrl: "https://jlg046.github.io/gisp-e-prep/index.html",
   supabasePublishableKey: "sb_publishable_JWwXaP3BxTx_9GZvXwQQ3w_9fKsXUBG"
 };
