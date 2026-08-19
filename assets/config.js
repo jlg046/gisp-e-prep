d54d597dc23f3f9dd29d2ee23c68ac674c2bfe92
Janine Glathar <jlg046@bucknell.edu>
2026-08-19 12:39:21 -0400

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 // Replace these two values with the values from your Supabase project.
 // Supabase Dashboard -> Project Settings -> API
 window.GISPE_CONFIG = {
-  supabaseUrl: "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE",
-  supabasePublishableKey: "PASTE_YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY_HERE"
+  supabaseUrl: "https://nxgaznpdocddjzqmjgtd.supabase.co/rest/v1/",
+  supabasePublishableKey: "sb_publishable_JWwXaP3BxTx_9GZvXwQQ3w_9fKsXUBG"
 };
